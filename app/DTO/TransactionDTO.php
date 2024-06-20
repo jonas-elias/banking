8be6ef456e79b869cf4c1757d44ef822d35b019d
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-20 00:18:28 +0000

    feat: skeleton DTO User/Transaction

---
+++ app/DTO/TransactionDTO.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DTO;
+
+use FriendsOfHyperf\ValidatedDTO\ValidatedDTO;
+
+class TransactionDTO extends ValidatedDTO
+{
+    public function messages(): array
+    {
+        return [];
+    }
+
+    public function attributes(): array
+    {
+        return [];
+    }
+
+    protected function rules(): array
+    {
+        return [];
+    }
+
+    protected function scenes(): array
+    {
+        return [];
+    }
+
+    protected function defaults(): array
+    {
+        return [];
+    }
+
+    protected function casts(): array
+    {
+        return [];
+    }
+}

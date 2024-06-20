454d5fddd1be7552674c96170fa5221a9a7ff118
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-20 04:49:47 +0000

    feat: added document exception

---
+++ app/Domain/User/Document/Exception/DocumentException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User\Document\Exception;
+
+use Exception;
+
+/**
+ * Exception class to invalid document
+ */
+class DocumentException extends Exception
+{
+}

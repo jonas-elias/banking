07c23e6dbab87ec82d26779d85737321dc1d62ce
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-21 04:59:47 +0000

    feat: route POST register user

--- config/routes.php
+++ config/routes.php
@@ -9,10 +9,8 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use App\Domain\User\Controller\UserController;
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
-
-Router::get('/favicon.ico', function () {
-    return '';
-});
+Router::post('/api/user/register', [UserController::class, 'register']);

b7a3cb28cd792612f54493550b5a78e1df9b243d
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-21 04:43:30 +0000

    feat: model common user type

---
+++ app/Domain/User/Common.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User;
+
+/**
+ * Model to represent user type common.
+ */
+class Common extends User
+{
+    /**
+     * @var $attributes
+     * Default values.
+     */
+    protected array $attributes = [
+        'type' => UserType::Common->value,
+    ];
+}

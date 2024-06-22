27eef969077d3b8cf130275271b770322ca107ae
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-22 21:44:04 +0000

    test: skeleton transaction DTO

---
+++ test/Domain/Transaction/DTO/TransactionDTOTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Domain\Transaction\DTO;
+
+use PHPUnit\Framework\TestCase;
+
+class TransactionDTOTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testTransactionDTOValidationRules(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionDTOValidationMessages(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionDTOCastTypes(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionDTODefaults(): void
+    {
+        $this->assertTrue(true);
+    }
+}

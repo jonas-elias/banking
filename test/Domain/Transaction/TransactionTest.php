c0030f4f13c91577111c9b86fde20fd3979553df
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-22 21:41:35 +0000

    test: skeleton transaction model

---
+++ test/Domain/Transaction/TransactionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Domain\Transaction;
+
+use PHPUnit\Framework\TestCase;
+
+class TransactionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testTransactionTableName(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionFillableAttributes(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionCasts(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionTimestamps(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionDates(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTransactionHasUlidsTrait(): void
+    {
+        $this->assertTrue(true);
+    }
+}

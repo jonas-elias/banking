4e295e6bb4d73fc35bd88a2df36833e46ed745a7
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-22 21:38:32 +0000

    test: skeleton transaction repository

---
+++ test/Domain/Transaction/Repository/TransactionRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Domain\Transaction\Repository;
+
+use PHPUnit\Framework\TestCase;
+
+class TransactionRepositoryTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testTransactionRepositoryCreate(): void
+    {
+        $this->assertTrue(true);
+    }
+}

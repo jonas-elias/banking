b7b14cd73e9207fb629b8b9ac2f92945530ac42d
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-23 05:07:24 +0000

    test: scope transaction model

--- test/Domain/Transaction/TransactionTest.php
+++ test/Domain/Transaction/TransactionTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace HyperfTest\Domain\Transaction;
 
+use App\Domain\Transaction\Transaction;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class TransactionTest extends TestCase
@@ -15,36 +17,36 @@ class TransactionTest extends TestCase
 
     protected function tearDown(): void
     {
+        Mockery::close();
         parent::tearDown();
     }
 
     public function testTransactionTableName(): void
     {
-        $this->assertTrue(true);
+        $transaction = $this->createTransactionMock();
+        $transaction->shouldReceive('getTable')->andReturn('transaction');
+
+        $this->assertEquals('transaction', $transaction->getTable());
     }
 
     public function testTransactionFillableAttributes(): void
     {
-        $this->assertTrue(true);
+        $transaction = $this->createTransactionMock();
+        $transaction->shouldReceive('getFillable')->andReturn(['id', 'payer_id', 'payee_id', 'value']);
+
+        $this->assertEquals(['id', 'payer_id', 'payee_id', 'value'], $transaction->getFillable());
     }
 
     public function testTransactionCasts(): void
     {
-        $this->assertTrue(true);
+        $transaction = $this->createTransactionMock();
+        $transaction->shouldReceive('getCasts')->andReturn(['created_at' => 'datetime', 'updated_at' => 'datetime']);
+
+        $this->assertEquals(['created_at' => 'datetime', 'updated_at' => 'datetime'], $transaction->getCasts());
     }
 
-    public function testTransactionTimestamps(): void
+    protected function createTransactionMock()
     {
-        $this->assertTrue(true);
-    }
-
-    public function testTransactionDates(): void
-    {
-        $this->assertTrue(true);
-    }
-
-    public function testTransactionHasUlidsTrait(): void
-    {
-        $this->assertTrue(true);
+        return Mockery::mock(Transaction::class)->makePartial();
     }
 }

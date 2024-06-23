c5e1479103030ef0f98e2e69f88447ba278b4102
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-23 05:07:43 +0000

    test: scope transaction service

--- test/Domain/Transaction/Service/TransactionServiceTest.php
+++ test/Domain/Transaction/Service/TransactionServiceTest.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace HyperfTest\Domain\Transaction\Service;
 
+use App\Database\DatabaseTransaction;
+use App\Domain\Transaction\DTO\TransactionDTO;
+use App\Domain\Transaction\Exception\PayeeException;
+use App\Domain\Transaction\Exception\PayerException;
+use App\Domain\Transaction\Repository\TransactionRepository;
+use App\Domain\Transaction\Service\TransactionService;
+use App\Domain\User\Repository\UserRepository;
+use App\Domain\User\User;
+use Exception;
+use Mockery;
+use Mockery\LegacyMockInterface;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
 
 class TransactionServiceTest extends TestCase
@@ -15,26 +27,156 @@ class TransactionServiceTest extends TestCase
 
     protected function tearDown(): void
     {
+        Mockery::close();
         parent::tearDown();
     }
 
     public function testTransactionServiceTransferSuccessful(): void
     {
+        $databaseTransaction = $this->createDatabaseTransactionMock();
+        $userRepository = $this->createUserRepositoryMock();
+        $transRepository = $this->createTransactionRepositoryMock();
+        $transactionDTO = $this->createTransactionDTOMock();
+        $user = $this->createUserMock();
+
+        $transactionDTO->payer = 'payer_id';
+        $transactionDTO->payee = 'payee_id';
+        $transactionDTO->value = 100;
+
+        $databaseTransaction->shouldReceive('executeTransaction')->andReturnUsing(function ($callable) {
+            $callable();
+        });
+        $userRepository->shouldReceive('findUsersByIds')
+            ->with([$transactionDTO->payer, $transactionDTO->payee])
+            ->andReturn([$transactionDTO->payer => $user, $transactionDTO->payee => $user]);
+        $user->shouldReceive('debit')->andReturnSelf();
+        $user->shouldReceive('credit')->andReturnSelf();
+        $userRepository->shouldReceive('save')->andReturnSelf();
+        $transRepository->shouldReceive('create')->andReturn([
+            'transaction' => 'transaction-id',
+        ]);
+
+        $service = new TransactionService($databaseTransaction, $transRepository, $userRepository);
+
+        $service->transfer($transactionDTO);
         $this->assertTrue(true);
     }
 
     public function testTransactionServiceTransferPayerException(): void
     {
-        $this->assertTrue(true);
+        $databaseTransaction = $this->createDatabaseTransactionMock();
+        $userRepository = $this->createUserRepositoryMock();
+        $transRepository = $this->createTransactionRepositoryMock();
+        $transactionDTO = $this->createTransactionDTOMock();
+        $user = $this->createUserMock();
+
+        $transactionDTO->payer = 'payer_id';
+        $transactionDTO->payee = 'payee_id';
+        $transactionDTO->value = 100;
+
+        $databaseTransaction->shouldReceive('executeTransaction')->andReturnUsing(function ($callable) {
+            $callable();
+        });
+        $userRepository->shouldReceive('findUsersByIds')
+            ->with([$transactionDTO->payer, $transactionDTO->payee])
+            ->andReturn([$transactionDTO->payee => $user]);
+        $user->shouldReceive('debit')->andReturnSelf();
+        $user->shouldReceive('credit')->andReturnSelf();
+        $userRepository->shouldReceive('save')->andReturnSelf();
+        $transRepository->shouldReceive('create')->andReturn([
+            'transaction' => 'transaction-id',
+        ]);
+
+        $service = new TransactionService($databaseTransaction, $transRepository, $userRepository);
+
+        $this->expectException(PayerException::class);
+        $service->transfer($transactionDTO);
     }
 
     public function testTransactionServiceTransferPayeeException(): void
     {
-        $this->assertTrue(true);
+        $databaseTransaction = $this->createDatabaseTransactionMock();
+        $userRepository = $this->createUserRepositoryMock();
+        $transRepository = $this->createTransactionRepositoryMock();
+        $transactionDTO = $this->createTransactionDTOMock();
+        $user = $this->createUserMock();
+
+        $transactionDTO->payer = 'payer_id';
+        $transactionDTO->payee = 'payee_id';
+        $transactionDTO->value = 100;
+
+        $databaseTransaction->shouldReceive('executeTransaction')->andReturnUsing(function ($callable) {
+            $callable();
+        });
+        $userRepository->shouldReceive('findUsersByIds')
+            ->with([$transactionDTO->payer, $transactionDTO->payee])
+            ->andReturn([$transactionDTO->payer => $user]);
+        $user->shouldReceive('debit')->andReturnSelf();
+        $user->shouldReceive('credit')->andReturnSelf();
+        $userRepository->shouldReceive('save')->andReturnSelf();
+        $transRepository->shouldReceive('create')->andReturn([
+            'transaction' => 'transaction-id',
+        ]);
+
+        $service = new TransactionService($databaseTransaction, $transRepository, $userRepository);
+
+        $this->expectException(PayeeException::class);
+        $service->transfer($transactionDTO);
     }
 
     public function testTransactionServiceTransferGeneralException(): void
     {
-        $this->assertTrue(true);
+        $databaseTransaction = $this->createDatabaseTransactionMock();
+        $userRepository = $this->createUserRepositoryMock();
+        $transRepository = $this->createTransactionRepositoryMock();
+        $transactionDTO = $this->createTransactionDTOMock();
+        $user = $this->createUserMock();
+
+        $transactionDTO->payer = 'payer_id';
+        $transactionDTO->payee = 'payee_id';
+        $transactionDTO->value = 100;
+
+        $databaseTransaction->shouldReceive('executeTransaction')->andReturnUsing(function ($callable) {
+            $callable();
+        });
+        $userRepository->shouldReceive('findUsersByIds')
+            ->with([$transactionDTO->payer, $transactionDTO->payee])
+            ->andReturn([$transactionDTO->payer => $user, $transactionDTO->payee => $user]);
+        $user->shouldReceive('debit')->andReturnSelf();
+        $user->shouldReceive('credit')->andReturnSelf();
+        $userRepository->shouldReceive('save')->andReturnSelf();
+        $transRepository->shouldReceive('create')->andReturnUsing(function () {
+            throw new Exception();
+        });
+
+        $service = new TransactionService($databaseTransaction, $transRepository, $userRepository);
+
+        $this->expectException(Exception::class);
+        $service->transfer($transactionDTO);
+    }
+
+    protected function createDatabaseTransactionMock(): DatabaseTransaction|LegacyMockInterface|MockInterface
+    {
+        return Mockery::mock(DatabaseTransaction::class);
+    }
+
+    protected function createTransactionRepositoryMock(): TransactionRepository|LegacyMockInterface|MockInterface
+    {
+        return Mockery::mock(TransactionRepository::class);
+    }
+
+    protected function createUserRepositoryMock(): UserRepository|LegacyMockInterface|MockInterface
+    {
+        return Mockery::mock(UserRepository::class);
+    }
+
+    protected function createTransactionDTOMock(): TransactionDTO|LegacyMockInterface|MockInterface
+    {
+        return Mockery::mock(TransactionDTO::class);
+    }
+
+    protected function createUserMock(): User|LegacyMockInterface|MockInterface
+    {
+        return Mockery::mock(User::class);
     }
 }

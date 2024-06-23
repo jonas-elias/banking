aae495f491aa4d33c6b092901772561982f9416a
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-23 05:15:29 +0000

    feat: exceptions transaction module

---
+++ app/Domain/Transaction/Exception/PayerException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Transaction\Exception;
+
+use Exception;
+
+/**
+ * Payer agent exception class.
+ */
+class PayerException extends Exception
+{
+}

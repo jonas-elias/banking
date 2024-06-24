19eb53e85c026efaebbbc5566d8f9d2a7be95934
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-24 06:59:21 +0000

    test: user merchant model

---
+++ test/Domain/User/MerchantTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Domain\User;
+
+use App\Domain\User\Exception\UserDebitException;
+use App\Domain\User\Merchant;
+use App\Domain\User\UserType;
+use PHPUnit\Framework\TestCase;
+
+class MerchantTest extends TestCase
+{
+    public function testMerchantHasCorrectType()
+    {
+        $merchant = new Merchant();
+
+        $this->assertEquals(UserType::Merchant->value, $merchant->type);
+    }
+
+    public function testMerchantDebitThrowsUserDebitException()
+    {
+        $this->expectException(UserDebitException::class);
+
+        $merchant = new Merchant();
+        $merchant->debit(1000);
+    }
+}

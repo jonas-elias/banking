54220fa3314a48ed1b98267ef54e517371975b6d
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-24 08:52:57 +0000

    fix: guzzle param errors

--- app/Job/NotificationEmailJob.php
+++ app/Job/NotificationEmailJob.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Job;
 
+use App\Constants\HttpStatus;
 use Hyperf\AsyncQueue\Job;
 use Hyperf\Context\ApplicationContext;
 use Hyperf\Guzzle\ClientFactory;
+use Psr\Log\LoggerInterface;
 
 use function Hyperf\Config\config;
 
@@ -40,14 +42,21 @@ class NotificationEmailJob extends Job
         $uri = config('notification.email.uri');
 
         $container = $this->app->getContainer();
+
         $clientHttp = $container->get(ClientFactory::class);
+        $logger = $container->get(LoggerInterface::class);
 
         $request = $clientHttp->create();
-        $request->request('POST', $uri, [
+        $response = $request->post($uri, [
             'json' => [
                 'recipient' => $this->recipient,
                 'message' => $this->message,
-            ]
+            ],
+            'http_errors' => false,
         ]);
+
+        if ($response->getStatusCode() !== HttpStatus::NO_CONTENT) {
+            $logger->error($response->getBody()->getContents());
+        }
     }
 }

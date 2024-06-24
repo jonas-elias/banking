125dcf94653fd200a84ed2ab2aa32e5d745061b9
jonas-elias <jonasdasilvaelias@gmail.com>
2024-06-24 05:46:54 +0000

    fix: conversion float 1.0 (example) to 100 cents

---
+++ app/DTO/Cast/IntegerConversionCast.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO\Cast;
+
+use FriendsOfHyperf\ValidatedDTO\Casting\Castable;
+use FriendsOfHyperf\ValidatedDTO\Exception\CastException;
+
+class IntegerConversionCast implements Castable
+{
+    /**
+     * Cast and conversion to cents.
+     *
+     * @param string $property
+     * @param mixed $value
+     *
+     * @return int
+     */
+    public function cast(string $property, mixed $value): int
+    {
+        if (! is_numeric($value) && $value !== '') {
+            throw new CastException($property);
+        }
+
+        return is_int($value) ? $value : (int) $value * 100;
+    }
+}
